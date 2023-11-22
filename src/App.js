b5340cd12b4f44f1dc88f005dc3ef0216bbd92a1
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2023-11-22 10:00:33 +0100

    Kesz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [isFetchPending, setFetchPending] = useState(false);
+  const [pizzaget, pizzaset] = useState([]);
+  useEffect(() => {
+    setFetchPending(true);
+    fetch("https://pizza.kando-dev.eu/Pizza")
+      .then((res) => res.json())
+      .then((data) => pizzaset(data));
+    setFetchPending(false);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isFetchPending ? (
+        <div className="spinner-border"></div>
+      ) : (
+        <div>
+          <h1 className="text-center">Pizzák</h1>
+          <div className="d-flex justify-content-around">
+            {pizzaget.map((pizza) => (
+              <div className="card">
+                <p>Pizza neve: {pizza.name}</p>
+                <b>Glutén mentes-e: {pizza.isGlutenFree ? "Igen" : "Nem"}</b>
+                <img height="150px" src={pizza.kepURL}></img>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
